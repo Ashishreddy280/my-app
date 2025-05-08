fb162f52a7364ab7fed41d9a000958a751908f4f
Ashishreddy280 <sudulaashish28@gmail.com>
2025-05-08 14:07:33 +0530

    routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboradComponent } from './dashborad/dashborad.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'home',component:HomeComponent},
+  {path:'dashboard', component:DashboradComponent,children:[{ path:'login',component:LoginComponent}]},
+ 
+ {path:'services',component:ServicesComponent},
+ {path:'**', component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboradComponent } from './dashborad/dashborad.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+import { ErrorComponent } from './error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboradComponent,
+    LoginComponent,
+    ServicesComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
